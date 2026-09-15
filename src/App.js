d1c147be8fdc09b297c1fcc1000253c9b1740677
Simeon <61100437+simeon2002@users.noreply.github.com>
2026-09-15 18:38:24 +0200

    Add item to cart and render cart list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import products from "./data.json";
 
 export default function App() {
   const productList = products;
   const [cartItems, setCartItems] = useState([]);
 
+  function handleAddItem(item) {
+    console.log(item);
+    console.log(cartItems);
+
+    const getItemIdx = (items, itemToFind) => items.findIndex(item => item.name === itemToFind.name);
+
+    setCartItems(cartItems => {
+      const itemIdx = getItemIdx(cartItems, item);
+
+      if (itemIdx === -1) return [...cartItems, { ...item, count: 1 }];
+
+      return cartItems.map(cartItem => (cartItem.name === item.name ? { ...cartItem, count: cartItem.count++ } : cartItem));
+    });
+  }
+
   return (
     <main className="app-container">
-      <ProductCategory category="dessert" productList={productList} />
+      <ProductCategory category="dessert" productList={productList} onAddItem={handleAddItem} />
       <Cart cartItems={cartItems} />
     </main>
   );
 }
 
-function ProductCategory({ category, productList }) {
+function ProductCategory({ category, productList, onAddItem }) {
   return (
     <section className="category">
       <h1 className="category__title">{category[0].toUpperCase() + category.slice(1)}</h1>
       <ul className="category__product-list">
         {productList.map(product => (
-          <ProductCard product={product} />
+          <ProductCard product={product} onAddItem={onAddItem} key={product.name} />
         ))}
       </ul>
     </section>
   );
 }
 
-function ProductCard({ product }) {
+function ProductCard({ product, onAddItem }) {
   return (
     <li className="category-item">
       <article className="product product--selected">
@@ -36,8 +51,9 @@ function ProductCard({ product }) {
             <source srcSet={product.image.tablet} media="(min-width: 48em)" />
             <img src={product.image.mobile} alt="Waffle with berries" className="product__image" />
           </picture>
-          <Button className="btn--add-to-cart">
-            <img src="./assets/images/icon-add-to-cart.svg" alt="" className="icon icon-add-to-cart" /> Add to Cart
+          <Button className="btn--add-to-cart" onClick={() => onAddItem(product)}>
+            <img src="./assets/images/icon-add-to-cart.svg" alt="" className="icon icon-add-to-cart" />
+            Add to Cart
           </Button>
         </div>
         <div className="product__product-details">
@@ -50,8 +66,12 @@ function ProductCard({ product }) {
   );
 }
 
-function Button({ children, className = "" }) {
-  return <button className={`btn ${className}`}>{children}</button>;
+function Button({ children, className = "", onClick = () => {} }) {
+  return (
+    <button className={`btn ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 function ItemCount() {}
@@ -90,25 +110,26 @@ function Cart({ cartItems }) {
 function CartList({ cartItems }) {
   return (
     <ul className="cart__items-list">
-      {cartItems.map}
-      <CartItem>Classic Tiramisu</CartItem>
-      <div className="cart__divider"></div>
-      <CartItem>Classic Tiramisu</CartItem>
-      <div className="cart__divider"></div>
-      <CartItem>Classic Tiramisu</CartItem>
-      <div className="cart__divider"></div>
+      {cartItems.map(item => (
+        <React.Fragment key={item.name}>
+          <CartItem item={item}>{item.name}</CartItem>
+          <div className="cart__divider"></div>
+        </React.Fragment>
+      ))}
     </ul>
   );
 }
 
-function CartItem({ children }) {
+function CartItem({ item, children }) {
+  const itemTotal = item.count * item.price;
+
   return (
     <li className="cart-item">
       <div className="cart-item__details">
         <h3 className="cart-item__title">{children}</h3>
-        <span className="cart-item__count">1x</span>
-        <span className="cart-item__price">@ $5.50</span>
-        <span className="cart-item__price-total">@ $5.50</span>
+        <span className="cart-item__count">{item.count}x</span>
+        <span className="cart-item__price">@ ${item.price}</span>
+        <span className="cart-item__price-total">@ ${itemTotal}</span>
       </div>
       <Button className="btn--remove">
         <img src="./assets/images/icon-remove-item.svg" alt="" className="icon icon-remove" />
